992c00e62f0a602022ef403c97ba4dfd9c178e56
Onelio Viera <ojvwebdesign@gmail.com>
2024-05-11 17:36:38 -0600

    chore: Update page and sidebar styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function page() {
   return (
-    <h1 className="text-red-500">
+    <h1 className="text-red-500 text-2xl mt-20">
       Home Page
     </h1>
   )
--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,7 +1,7 @@
 export default function PostPage() {
   return (
-    <div>
-      PostPage
-    </div>
+<h1 className="text-red-500 text-2xl mt-20">
+      Post Page
+    </h1>
   )
 }
